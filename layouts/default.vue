717bd1e222ff04bc584b0ac8e3f119def3980bb2
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-04-14 12:56:33 +0200

    ajout des dossier

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+  <div class="flex md:flex-row flex-col m-4 gap-4">
+
+    <div class="flex-auto md:w-[70%] w-full">
+      <slot />
+    </div>
+
+    <div class="flex-auto md:w-[30%] w-full">
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
