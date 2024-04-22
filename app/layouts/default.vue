61c904275893f5ea8f57be6c23e56365ecd4bf91
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-04-22 11:49:58 +0200

    fix app config

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,15 +6,7 @@
 
   <div class="mx-40 ">
     <section class="relative w-full">
-      <div class="absolute top-0 inset-x-0 h-64 flex items-start">
-        <div
-            class="h-24 w-2/3 bg-gradient-to-br from-venice-blue-500 opacity-20 blur-2xl dark:from-[#570cac] dark:invisible dark:opacity-40">
-        </div>
-        <div
-            class="h-20 w-3/5 bg-gradient-to-r from-venice-blue-500 opacity-40 blur-2xl dark:from-[#670ccf] dark:opacity-40">
-        </div>
-      </div>
-      <div class="w-full px-5 sm:px-8 md:px-12 lg:px-8 max-w-screen-lg lg:max-w-screen-xl mx-auto " :class="className">
+      <div class="w-full px-5 sm:px-8 md:px-12 lg:px-8 max-w-screen-lg lg:max-w-screen-xl mx-auto">
         <OrganismsNavbar />
       </div>
 
