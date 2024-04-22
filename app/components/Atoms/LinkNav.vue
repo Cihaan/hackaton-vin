b793c2347f06a6c808a325533ffda5f8bb8647d5
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-04-22 22:09:56 +0200

    wip page accueil

---
+++ app/components/Atoms/LinkNav.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  to: string
+}>()
+
+</script>
+
+<template>
+
+  <nuxt-link class="text-red-wine-500 font-bold" :to="to">
+    <slot />
+  </nuxt-link>
+
+</template>
+
+<style scoped>
+
+</style>
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/ui", "@nuxt/image"],
+  tailwindcss: {
+    config : {
+      theme: {
+        extend: {
+          colors: {
+            'red-wine': {
+              '50': '#fdf4f3',
+              '100': '#fce8e7',
+              '200': '#f8d4d3',
+              '300': '#f2afaf',
+              '400': '#ea8284',
+              '500': '#de555b',
+              '600': '#bc313e',
+              '700': '#a92737',
+              '800': '#8e2334',
+              '900': '#7a2131',
+              '950': '#430e16',
+            },
+            'background' : '#FFFEED',
+            'black' : '#051822'
+          }
+        }
+      },
+    },
+  },
+  ui: {
+    global: true,
+    icons: [],
+  },
 })
