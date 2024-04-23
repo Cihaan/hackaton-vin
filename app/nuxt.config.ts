f7a3a7469d364a1320d5cfe72cebde3ebc98bc7b
mohamedDev69 <mohamedchettah0208@gmail.com>
2024-04-23 11:07:49 +0200

    ajout de pinia

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ["@nuxt/ui", "@nuxt/image"],
+  modules: ["@nuxt/ui", "@nuxt/image","@pinia/nuxt"],
   tailwindcss: {
     config : {
       theme: {
