cd01d4e4a4c666d1f4ce6642748d9d98893c55f3
Paul <paul.niggli25@gmail.com>
2024-04-23 13:59:16 +0200

    Paragraphe presentation de l'animateur

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,7 +7,7 @@
     <div class="flex flex-col py-32 px-20 gap-24">
       <div>
         <h1 class="text-white text-7xl leading-tight"> Découverte et <br> Dégustation <br> des Vins </h1>
-        <h3  class="text-white text-3xl mt-8">Un Voyage à travers les Saveurs</h3>
+        <h3 class="text-white text-3xl mt-8">Un Voyage à travers les Saveurs</h3>
       </div>
 
       <UButton
@@ -21,6 +21,29 @@
       />
     </div>
   </section>
+  <section class="animateur">
+    <div class="flex pr-96 pt-10 pl-96">
+      <div class="w-1/2 pr-36 mt-4">
+        <p class="text-5xl">Animateur</p>
+        <img src="../public/img/animateur.png" class="mt-10 max-w-max	mb-10" alt="bg max">
+      </div>
+      <div>
+        <p class="text-2xl leading-relaxed mt-24">
+          Plongez dans l'univers passionnant du vin avec notre hôte passionné, un connaisseur émérite désireux de
+          partager sa fascination pour les vins de toute la France. À travers des dégustations soigneusement
+          orchestrées, vous découvrirez un panorama des saveurs et des terroirs, des coteaux de Bordeaux aux vallées de
+          la Loire.
+        </p>
+        <p class="text-2xl leading-relaxed mt-12">
+          Que vous soyez novice ou amateur éclairé, chaque dégustation offre une expérience sensorielle enrichissante,
+          où les vins sélectionnés avec soin révèlent les secrets des vignobles français. Rejoignez-nous pour une
+          exploration vinicole captivante, où le plaisir de découvrir de nouveaux crus et de partager des moments
+          conviviaux est aussi abondant que les grappes mûres à la vendange.
+        </p>
+      </div>
+    </div>
+  </section>
+
 
 </template>
 
@@ -29,6 +52,14 @@
 .accueil {
   position: relative; /* Contains the pseudo-element */
   min-height: 700px; /* Matches the height of the pseudo-element */
+}
+
+.animateur {
+  position: relative; /* Contains the pseudo-element */
+  min-height: 700px; /* Matches the height of the pseudo-element */
+  background-color: #BC313E;
+  color: white;
+
 }
 
 .accueil::before {
