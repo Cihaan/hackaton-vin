5bd6b9f97d28b2cf990fbe1c3c4b64d063ddaa38
Cihaan <cihankafadar1@gmail.com>
2024-04-24 16:36:25 +0200

    fix: midlleware only on client side

--- app/middleware/guard-global.ts
+++ app/middleware/guard-global.ts
@@ -1,7 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+    if (!process.client) return;
     if (to.meta.requiresAuth) {
         try {
             const isLoggedIn = localStorage.getItem('user');
+            console.log(isLoggedIn);
             if (!isLoggedIn) return navigateTo('/login');
         } catch (e) {
             console.error('Error while checking session:', e);
