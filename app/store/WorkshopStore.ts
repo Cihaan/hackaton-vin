9c0062e2a84a10338c09f2acb1518cecc7b5f2d6
ilyasZK <ilyaszahaf2002@gmail.com>
2024-04-25 09:30:51 +0200

    <feat> composant card workshop

--- app/store/WorkshopStore.ts
+++ app/store/WorkshopStore.ts
@@ -3,14 +3,35 @@ import type {WorkshopType} from "~/types/WorkshopType";
 
 export const useWorkshopStore = defineStore('list-workshop', () => {
     const listWorkshop = ref<WorkshopType[]>([])
+    const workshopDetail = ref<WorkshopType | null>(null)
 
-    async function getWorkShop() {
-        const {data, pending, error, refresh} = await useFetch<WorkshopType[]>('http://127.0.0.1:8000/api/workshops', {})
-        if(data.value){
+    async function getWorkShops() {
+        const {
+            data,
+            pending,
+            error,
+            refresh
+        } = await useFetch<WorkshopType[]>('http://127.0.0.1:8000/api/workshops', {})
+        if (data.value) {
             // @ts-ignore
             listWorkshop.value = data.value['hydra:member']
         }
     }
 
-    return { listWorkshop, getWorkShop }
+    async function getWorkShop(id: string | string[]) {
+        const {
+            data,
+            pending,
+            error,
+            refresh
+        } = await useFetch<WorkshopType>('http://127.0.0.1:8000/api/workshops/' + id, {})
+        if (data.value) {
+            // @ts-ignore
+            workshopDetail.value = data.value
+            return;
+        }
+        workshopDetail.value = null
+    }
+
+    return {listWorkshop, getWorkShops, getWorkShop, workshopDetail}
 })
